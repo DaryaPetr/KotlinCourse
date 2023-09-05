f42a93165e97843eb8de5cdbff598753074923b6
userD <petrova077@mail.ru>
2023-09-05 21:53:46 +0700

    KC-1-1 add types

--- src/main/kotlin/lesson_1/les_1_task_1.kt
+++ src/main/kotlin/lesson_1/les_1_task_1.kt
@@ -2,7 +2,7 @@ package lesson_1
 
 fun main() {
 
-    var orderCount = 75
-    val gratitudeText = "Спасибо за покупку!"
+    var orderCount: Int = 75
+    val gratitudeText: String = "Спасибо за покупку!"
 
 }
