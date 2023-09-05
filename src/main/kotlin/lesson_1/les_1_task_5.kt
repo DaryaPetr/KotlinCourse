22a021b7965911fe9fb48cd9b9a0b124ee8a2684
userD <petrova077@mail.ru>
2023-09-05 23:55:43 +0700

    KC-1-5 add remainders

--- src/main/kotlin/lesson_1/les_1_task_5.kt
+++ src/main/kotlin/lesson_1/les_1_task_5.kt
@@ -1,13 +1,13 @@
 package lesson_1
 
 fun main() {
-    var second = 108 * 60
-    var minute = second / 60
+    val second = 6480
+    val secondRemainder = second % 60
+    val minute = second / 60 % 60
+    val minuteRemainder = minute % (60 * 60)
     val hour = second / 60 / 60
-    minute -= hour * 60
-    second = second - hour * 60 * 60 - minute * 60
     hour.toString()
-    minute.toString()
-    second.toString()
-    println(String.format("%02d:%02d:%02d", hour, minute, second))
+    minuteRemainder.toString()
+    secondRemainder.toString()
+    println(String.format("%02d:%02d:%02d", hour, minuteRemainder, secondRemainder))
 }
