747759218d0e9bdcdb2592a730e5e045eaa23ea6
userD <petrova077@mail.ru>
2023-09-05 23:02:43 +0700

    KC-1-3 add and print values from task 3

---
+++ src/main/kotlin/lesson_1/les_1_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("Взлёт:\n" +
+            "$year\n" +
+            "$hour\n" +
+            "$minute")
+    hour = 10
+    minute = 55
+    println("Посадка: $hour:$minute")
+}
