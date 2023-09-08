1f3f0ed70c64d69a65f88f7d37bc78b9d4294f3b
userD <petrova077@mail.ru>
2023-09-08 22:27:10 +0700

    KC-3-2 change data storage and printing

--- src/main/kotlin/lesson_3/les_3_task_2.kt
+++ src/main/kotlin/lesson_3/les_3_task_2.kt
@@ -1,42 +1,20 @@
 package lesson_3
 
-import java.time.LocalDate
-import java.time.Period
-import java.time.format.DateTimeFormatter
-
 fun main() {
 
-    val dateFormatter: DateTimeFormatter =  DateTimeFormatter.ofPattern("dd.MM.yyyy")
+    val surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
 
-    val birthday = LocalDate.parse("01.01.2000", dateFormatter)
+    val surnameChangeAge = 22
+    val newSurname = "Сидорова"
 
-    val surname = mutableMapOf(birthday to "Андреева")
-    val name = mutableMapOf(birthday to "Татьяна")
-    val patronymic = mutableMapOf(birthday to "Сергеевна")
-    val personData = listOf(surname, name, patronymic)
-
-    surname.put(LocalDate.parse("06.04.2022", dateFormatter), "Сидорова")
-
-    printFullNameAndAge(birthday, personData, LocalDate.parse("02.01.2020", dateFormatter))
-    printFullNameAndAge(birthday, personData, LocalDate.parse("01.08.2022", dateFormatter))
-
+    var age = 20
+    println("${computeSurname(surname, newSurname, surnameChangeAge, age)} $name $patronymic, $age")
+    age = 22
+    println("${computeSurname(surname, newSurname, surnameChangeAge, age)} $name $patronymic, $age")
 }
 
-fun printFullNameAndAge(birth: LocalDate, personData: List<MutableMap<LocalDate, String>>,
-                        requestedDate: LocalDate) {
-    var fullName = ""
-    for (item in personData){
-        fullName += "${getData(item, requestedDate)} "
-    }
-    fullName = fullName.dropLast(1)
-    val period = Period.between(birth, requestedDate)
-    val age = period.years
-    println("$fullName, $age")
-}
-
-fun getData(dataMap: MutableMap<LocalDate, String>, requestedDate: LocalDate): String {
-    var name = ""
-    for ((date, personName) in dataMap)
-        if (date <= requestedDate) name = personName else break
-    return name
-}
+fun computeSurname(surname: String, newSurname: String,
+                   surnameChangeAge: Int, age: Int) =
+    if (age < surnameChangeAge) surname else newSurname
