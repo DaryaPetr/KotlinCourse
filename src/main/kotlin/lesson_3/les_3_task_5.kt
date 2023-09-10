079d4a3f3b8bdb7bd1156e5fee7d98e6439d7a36
userD <petrova077@mail.ru>
2023-09-10 23:45:32 +0700

    KC-3-5 remove comments

--- src/main/kotlin/lesson_3/les_3_task_5.kt
+++ src/main/kotlin/lesson_3/les_3_task_5.kt
@@ -2,10 +2,10 @@ package lesson_3
 
 fun main() {
     val move = "D2-D4;0"
-//    var regex = """\W""".toRegex()
-//    regex = """[^A-Z0-9]""".toRegex()
-//    val regex = """[-;]""".toRegex()
-    val list = move.split("""[-;]""".toRegex())
+    var regex = """\W""".toRegex()
+    regex = """[^A-Z0-9]""".toRegex()
+    regex = """[-;]""".toRegex()
+    val list = move.split(regex)
     val from = list[0]
     val to = list[1]
     val moveNumber = list[2]
