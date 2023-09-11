b9b9b86f347a406343398ea7db1877692d9981c3
userD <petrova077@mail.ru>
2023-09-11 23:08:45 +0700

    KC-5-2 check if user old enough

---
+++ src/main/kotlin/lesson_5/les_5_task_2.kt
@@ -0,0 +1,14 @@
+package lesson_5
+
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+
+    val currentYear = 2023
+    print("Введите год рождения: ")
+    val birthYear = readLine()!!.toInt()
+
+    if (currentYear - birthYear >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
+    else println("Вернуться на главный экран")
+
+}
