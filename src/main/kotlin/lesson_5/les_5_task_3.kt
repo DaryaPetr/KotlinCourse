ac48d4700fcef43e0f894806420dac3b97a6f266
userD <petrova077@mail.ru>
2023-09-12 00:33:34 +0700

    KC-5-3 fixed bug with reading one win number

--- src/main/kotlin/lesson_5/les_5_task_3.kt
+++ src/main/kotlin/lesson_5/les_5_task_3.kt
@@ -4,23 +4,26 @@ fun main() {
 
     val firstWinNum = 43
     val secondWinNum = 89
-    val winNumbers = listOf(firstWinNum, secondWinNum)
 
     println("Загадано 2 числа от 1 до 100. У вас есть 5 попыток, чтобы их угадать")
+    println()
 
-    var guessedNumCount = 0
+    var firstGuessed = false
+    var secondGuessed = false
 
     for (i in 1..5) {
         print("Попытка $i: Введите число ")
         val num = readLine()!!.toInt()
-        if (winNumbers.contains(num)) guessedNumCount++
-        if (guessedNumCount == 2) break
+        if (num == firstWinNum) firstGuessed = true
+        if (num == secondWinNum) secondGuessed = true
+        if (firstGuessed && secondGuessed) break
     }
+    println()
 
-    when (guessedNumCount) {
-        0 -> println("Неудача! Крутите барабан!")
-        1 -> println("Вы выиграли утешительный приз!")
-        else -> println("Поздравляем! Вы выиграли главный приз!")
-    }
+    if (firstGuessed && secondGuessed) println("Поздравляем! Вы выиграли главный приз!")
+    else if (firstGuessed || secondGuessed) println("Вы выиграли утешительный приз!")
+    else println("Неудача! Крутите барабан!")
+    println()
+
     println("Загаданные числа: $firstWinNum, $secondWinNum")
 }
