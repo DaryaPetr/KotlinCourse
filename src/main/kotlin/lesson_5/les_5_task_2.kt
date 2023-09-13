7b81e2886cb6c228ea1ff75a956f1a83f34793f0
userD <petrova077@mail.ru>
2023-09-13 23:03:43 +0700

    KC-5-1 change readLine() to readln()

--- src/main/kotlin/lesson_5/les_5_task_2.kt
+++ src/main/kotlin/lesson_5/les_5_task_2.kt
@@ -6,7 +6,7 @@ fun main() {
 
     val currentYear = 2023
     print("Введите год рождения: ")
-    val birthYear = readLine()!!.toInt()
+    val birthYear = readln().toInt()
 
     if (currentYear - birthYear >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
     else println("Вернуться на главный экран")
