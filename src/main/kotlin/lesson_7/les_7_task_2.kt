5f8e760133113d711c1678ffd242b17db2bd19ed
userD <petrova077@mail.ru>
2023-09-27 01:33:41 +0700

    KC-7-2 change while() to do while()

--- src/main/kotlin/lesson_7/les_7_task_2.kt
+++ src/main/kotlin/lesson_7/les_7_task_2.kt
@@ -2,15 +2,13 @@ package lesson_7
 
 fun main() {
 
-    while (true) {
+     do {
         val code = (1000..9999).random()
         println("Ваш код авторизации: $code")
         print("Введите код авторизации ")
         val data = readln().toInt()
-        if (code == data) {
-            println("Добро пожаловать!")
-            break
-        } else println("Неверный код")
-    }
+        if (code != data) println("Неверный код\n")
+    } while (code != data)
+    println("Добро пожаловать!")
 
 }
