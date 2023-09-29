f096f5607a771fae233dd8f21890973ed11d353b
userD <petrova077@mail.ru>
2023-09-29 19:36:42 +0700

    KC-8-5 changed array initialization

--- src/main/kotlin/lesson_8/les_8_task_5.kt
+++ src/main/kotlin/lesson_8/les_8_task_5.kt
@@ -4,11 +4,9 @@ fun main() {
 
     println("Сколько будет ингредиентов?")
     val ingredientsCount = readln().toInt()
-    val ingredients = Array<String>(ingredientsCount) {""}
-
-    for (i in 0 until ingredientsCount) {
-        print("Ингредиент, который нужно добавить: ")
-        ingredients[i] = readln()
+    val ingredients = Array<String>(ingredientsCount) {
+            print("Ингредиент, который нужно добавить: ")
+            readln()
     }
 
     for (i in ingredients) println(i)
