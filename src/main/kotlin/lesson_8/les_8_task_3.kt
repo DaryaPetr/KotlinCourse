18920de0b250caff9969e23203a23753040169e0
userD <petrova077@mail.ru>
2023-09-29 19:28:09 +0700

    KC-8-2 changed check

--- src/main/kotlin/lesson_8/les_8_task_3.kt
+++ src/main/kotlin/lesson_8/les_8_task_3.kt
@@ -5,7 +5,7 @@ fun main() {
     val saladIngredients = arrayOf("помидор", "огурец", "лук", "соль")
     println("Какой ингидиент вы хотите найти в рецепте?")
     val ingredient = readln()
-    if (saladIngredients.contains(ingredient)) println("Ингредиент [$ingredient] в рецепте есть")
+    if (ingredient in saladIngredients) println("Ингредиент [$ingredient] в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
 
 }
