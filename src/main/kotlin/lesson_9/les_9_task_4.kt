3eeb12d07841e0a4bf29c37fdc8531cbc0109f06
userD <petrova077@mail.ru>
2023-10-03 01:01:35 +0700

    KC-9-4 sorts list of ingredient given by user

--- src/main/kotlin/lesson_9/les_9_task_4.kt
+++ src/main/kotlin/lesson_9/les_9_task_4.kt
@@ -2,7 +2,7 @@ package lesson_9
 
 fun main() {
 
-    println("Введите все ингредиенты черз запятую")
+    println("Введите все ингредиенты через запятую")
     val ingredients = readln().split(',').sorted()
     println("Ингредиенты: $ingredients")
 
