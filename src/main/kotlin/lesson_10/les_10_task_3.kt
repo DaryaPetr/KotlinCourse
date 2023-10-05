c70aa461c7ce2802c37665b3288f13c44c87a6fa
userD <petrova077@mail.ru>
2023-10-05 22:16:05 +0700

    KC-10-3 add special symbols and numbers alternation

--- src/main/kotlin/lesson_10/les_10_task_3.kt
+++ src/main/kotlin/lesson_10/les_10_task_3.kt
@@ -1,7 +1,9 @@
 package lesson_10
 
-const val FIRST_ACCEPT_SYMBOL = 32
-const val LAST_ACCEPT_SYMBOL = 57
+const val FIRST_SPECIAL_SYMBOL = 32
+const val LAST_SPECIAL_SYMBOL = 47
+const val FIRST_NUM_SYMBOL = 48
+const val LAST_NUM_SYMBOL = 57
 
 fun main() {
 
@@ -13,6 +15,10 @@ fun main() {
 
 fun generatePassword(length: Int): String {
     var pas = ""
-    for (i in 1..length) pas += (FIRST_ACCEPT_SYMBOL..LAST_ACCEPT_SYMBOL).random().toChar()
+    for (i in 1..length)
+        pas += when (i % 2) {
+            0 -> (FIRST_SPECIAL_SYMBOL..LAST_SPECIAL_SYMBOL).random().toChar()
+            else -> (FIRST_NUM_SYMBOL..LAST_NUM_SYMBOL).random().toChar()
+        }
     return pas
 }
