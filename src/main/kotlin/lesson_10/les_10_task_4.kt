f62182ab5c44d7bf194e2dbc0a097c0ad91a3566
userD <petrova077@mail.ru>
2023-10-08 23:27:34 +0700

    KC-10-4 add "Ничья"

--- src/main/kotlin/lesson_10/les_10_task_4.kt
+++ src/main/kotlin/lesson_10/les_10_task_4.kt
@@ -4,16 +4,15 @@ fun main() {
 
     var winCount = 0
     do {
-        do {
-            val userRes = rollDice1()
-            val computerRes = rollDice1()
-            println("Человек: $userRes, Компьютер: $computerRes")
-            if (userRes > computerRes) {
-                println("Победило человечество")
-                winCount++
-            }
-            else if (userRes < computerRes) println("Победила машина")
-        } while (userRes == computerRes)
+        val userRes = rollDice1()
+        val computerRes = rollDice1()
+        println("Человек: $userRes, Компьютер: $computerRes")
+        if (userRes > computerRes) {
+            println("Победило человечество")
+            winCount++
+        }
+        else if (userRes < computerRes) println("Победила машина")
+        else println("Ничья")
     } while (isContinue())
     println("Человек выиграл $winCount партий")
 
