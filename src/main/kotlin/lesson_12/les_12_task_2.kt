b242c595a97253d23ac6750362c265ce8e9e8082
userD <petrova077@mail.ru>
2023-10-10 18:50:22 +0700

    KC-12-2 create class with primary constructor and initialization in class

---
+++ src/main/kotlin/lesson_12/les_12_task_2.kt
@@ -0,0 +1,29 @@
+package lesson_12
+
+class Weather2(
+    val _dayTemperature: Int,
+    val _nightTemperature: Int,
+    val _isRain: Boolean,
+    val _atmospherePressure: Int,
+) {
+    var dayTemperature = _dayTemperature
+    var nightTemperature = _nightTemperature
+    var isRain: Boolean = _isRain
+    var atmospherePressure = _atmospherePressure
+
+    fun printData() {
+        println("dayTemperature $dayTemperature, nightTemperature $nightTemperature, " +
+                "isRain $isRain, atmospherePressure $atmospherePressure")
+    }
+}
+
+fun main() {
+
+    val weather1 = Weather2(20, -2, false, 750)
+    val weather2 = Weather2(13, 0, true, 740)
+    val weather3 = Weather2(19, 6, true, 755)
+    weather1.printData()
+    weather2.printData()
+    weather3.printData()
+
+}
