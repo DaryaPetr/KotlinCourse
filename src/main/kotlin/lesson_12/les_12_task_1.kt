acdfbc243a69821263287bb7f5cd8e8116a2ee14
userD <petrova077@mail.ru>
2023-10-10 18:34:47 +0700

    KC-12-1 create class with empty constructor

---
+++ src/main/kotlin/lesson_12/les_12_task_1.kt
@@ -0,0 +1,26 @@
+package lesson_12
+
+class Weather() {
+    var dayTemperature = 10
+    var nightTemperature = 1
+    var isRain = false
+    var atmospherePressure = 700
+
+    fun printData() {
+        println("dayTemperature $dayTemperature, nightTemperature $nightTemperature, " +
+                "isRain $isRain, atmospherePressure $atmospherePressure")
+    }
+}
+
+fun main() {
+
+    val weather1 = Weather()
+    val weather2 = Weather()
+    weather1.printData()
+    weather2.printData()
+    weather1.dayTemperature = 20
+    weather2.isRain = true
+    weather1.printData()
+    weather2.printData()
+
+}
